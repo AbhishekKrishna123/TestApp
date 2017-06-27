a623195746ec2b6162bf169099d7dde9841c0ddf
Abhishek Krishna <abhishekkrishna123@outlook.com>
2017-06-28 02:09:29 +0530

    2:09

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/slash', function(req, res) {
             // Formatting
             var obj = {
                 "pretext": "*Highlight #" + highlightNumber + "* from *" + postName + "* by _" + postAuthor + "_",
-                "text": finalText,
+                "text": "" + finalText + "",
                 "callback_id" : "button-trial",
                 "mrkdwn_in": [
                     "text",
