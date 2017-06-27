26eeea6f3d252130037b936cd1ecde18aedd3855
Abhishek Krishna <abhishekkrishna123@outlook.com>
2017-06-27 22:13:35 +0530

    73

--- entity.js
+++ entity.js
@@ -5,8 +5,10 @@ module.exports = {
 
         var tableSvc = azure.createTableService();
 
+        response.send("Hi!");
+
         tableSvc.retrieveEntity('TestTable', 'User', "request.query.MediumName", function(error, result, response){
-            response.send("Hi!");
+            
             if(!error){
                 response.send(response);
                 // result contains the entity
