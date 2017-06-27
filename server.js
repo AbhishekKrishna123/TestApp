306a40d262563755f30010b958666a7206e8c60f
Abhishek Krishna <abhishekkrishna123@outlook.com>
2017-06-28 01:54:53 +0530

    Bold

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post('/slash', function(req, res) {
             // Formatting
             var obj = {
                 "pretext": "*Highlight #" + highlightNumber + "* from *" + postName + "* by _" + postAuthor + "_",
-                "text": ">" + startingPart + "`" + quote + "`" + endingPart,
+                "text": startingPart + "*" + quote + "*" + endingPart,
                 "callback_id" : "button-trial",
                 "mrkdwn_in": [
                     "text",
