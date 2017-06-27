6cd418f9b18719f9f1c1a9d9b4f61ff918599c7d
Abhishek Krishna <abhishekkrishna123@outlook.com>
2017-06-27 20:34:25 +0530

    66

--- entity.js
+++ entity.js
@@ -7,6 +7,7 @@ module.exports = {
 
         tableSvc.retrieveEntity('TestTable', 'User', request.query.MediumName, function(error, result, response){
             if(!error){
+                response.send(response);
                 // result contains the entity
                 response.send(result.MediumUserID + ", " + result.DisplayName);
             }
