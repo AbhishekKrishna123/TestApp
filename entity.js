2d1363d1db8022393d8b7b89dec7919669a4dd87
Abhishek Krishna <abhishekkrishna123@outlook.com>
2017-06-27 23:04:31 +0530

    74

--- entity.js
+++ entity.js
@@ -5,18 +5,30 @@ module.exports = {
 
         var tableSvc = azure.createTableService();
 
-        response.send("Hi!");
-
-        tableSvc.retrieveEntity('TestTable', 'User', "request.query.MediumName", function(error, result, response){
-            
-            if(!error){
-                response.send(response);
-                // result contains the entity
-                response.send(result.MediumUserID + ", " + result.DisplayName);
+        var query = new azure.TableQuery()
+        .top(5)
+        .where('PartitionKey eq ?', 'User');
+        
+        tableSvc.queryEntities('mytable',query, null, function(error, result, response) {
+            if(!error) {
+                // query was successful
+                response.send("Success");
             }
             else {
-                response.send(response);
+                response.send("Fail");
             }
         });
+
+        // tableSvc.retrieveEntity('TestTable', 'User', "request.query.MediumName", function(error, result, response){
+            
+        //     if(!error){
+        //         response.send(response);
+        //         // result contains the entity
+        //         response.send(result.MediumUserID + ", " + result.DisplayName);
+        //     }
+        //     else {
+        //         response.send(response);
+        //     }
+        // });
     }
 }
