95d6de6ccbbac7bf5a7c2071263f26c101f4cfa8
Abhishek Krishna <abhishekkrishna123@outlook.com>
2017-06-28 19:48:25 +0530

    search functionality added

--- search.js
+++ search.js
@@ -1,12 +1,13 @@
 module.exports = {
     // Search through the user's highlights and return the most relevant posts
-    // PARAMETERS: MediumName, Search Term
+    // PARAMETERS: MediumName, Keyword
     Search: function (req, res) {
 
         // require this stuff
         var azure = require("azure-storage");
 
         var MediumName = req.query.MediumName;
+        var Keyword = req.query.Keyword;
 
         // Initialize Azure Table Service
         var tableSvc = azure.createTableService();
@@ -23,6 +24,41 @@ module.exports = {
                     if (!error) {
                         // query was successful
                         res.send("Number of entries: " + result.entries.length);
+
+                        // Analyse and find 5 posts most relevant to the keyword
+
+                        var relevanceScoreArray = [];
+                        var highlightObjects = result.entries;
+
+                        for (var i=0; i<highlightObjects.length; i++) {
+                            var score = 1;
+
+                            score += countOcurrences(highlightObjects[i].Paragraph, Keyword);
+                            score += countOcurrences(highlightObjects[i].PostName, Keyword);
+                            score += countOcurrences(highlightObjects[i].PostAuthor, Keyword);
+
+                            relevanceScoreObject = {
+                                "index": i,
+                                "score": score
+                            }
+
+                            relevanceScoreArray.push(relevanceScoreObject);
+                        }
+
+                        // Sorts the score array in descending order
+                        relevanceScoreArray.sort(function (a, b) {
+                            return parseInt(b.score) - parseInt(a.score);
+                        });
+
+                        // Return upto top 5 results
+                        var responseString = "";
+
+                        for (var i=0; i < min(5, highlightObjects.length); i++) {
+                            responseString += JSON.stringify(highlightObjects[relevanceScoreArray[i].index]);
+                        }
+
+                        res.send(responseString);
+
                     } else {
                         res.send(response);
                     }
@@ -34,3 +70,8 @@ module.exports = {
         });
     }
 };
+
+function countOcurrences(str, key) {
+    var regExp = new RegExp(key, "gi");
+    return (str.match(regExp) || []).length;
+}
