e264a36fa6ff16d94a689dad4148a49e5e20808f
Aditya Giridharan <aditya.giridharan@gmail.com>
2017-06-28 12:58:40 +0530

    giri1258

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/actions', urlencodedParser, (req, res) =>{
     res.status(200).end() // best practice to respond with 200 status
     var actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
 
-    var index = parseInt(actionJSONPayload.actions.name[7])
+    //var index = parseInt(actionJSONPayload.actions.name[6])
 
     var message = {
         "response_type" : "in_channel",
